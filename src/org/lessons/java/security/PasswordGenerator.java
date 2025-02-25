2e884ea09d83cb842630325df93efa803fe68825
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-02-25 17:27:40 +0100

    add output password

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,30 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        
+
+        // DICHIARO LE VARIABILI
+        String name;
+        String surname;
+        String favouriteColor;
+        int day;
+        int month;
+        int year;
+
+        // RECUPERO I DATI
+        Scanner inputUser = new Scanner(System.in);
+        name = inputUser.nextLine();
+        surname = inputUser.nextLine();
+        favouriteColor = inputUser.nextLine();
+        day = inputUser.nextInt();
+        month = inputUser.nextInt();
+        year = inputUser.nextInt();
+
+
+        // STAMPO LA PASSWORD
+        System.out.println("Password: " + name + surname + favouriteColor + (day + month + year));
+
     }
 }
